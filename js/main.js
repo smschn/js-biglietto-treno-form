f083eccd35740ea74a799aa1f9d22f7de448252a
Luca Somaschini <somas99@gmail.com>
2022-07-05 14:55:06 +0200

    Working on js - added variables

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+// dichiarazione e assegnazione variabili
+const percorsoKm = prompt('Quanti km vuoi percorrere?');
+const etaPasseggero = prompt('Quanti anni hai?')
+const prezzoKm = 0.21;
+const scontoMinorenni = 0.8;
+const scontoAnziani = 0.6;
+let prezzoBiglietto = percorsoKm * prezzoKm;
+
+
+
 // debug
 console.log('test')
+console.log('prezzoBiglietto')
