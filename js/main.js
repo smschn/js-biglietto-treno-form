cf4b5f8b3a143f8a6a94ece58ae1ba2497f15dc7
Luca Somaschini <somas99@gmail.com>
2022-07-05 17:30:47 +0200

    Worked on js

--- js/main.js
+++ js/main.js
@@ -6,14 +6,36 @@ let distance = document.getElementById('distance');
 let fasciaEta = document.getElementById('age_list');
 let button = document.getElementById('genera');
 
+let nomePasseggero = document.getElementById('nome-passeggero');
+let offerType = document.getElementById('offerta');
+
 // cosa succede al click del bottone
 button.addEventListener('click',
 function() {
+
     // check inserimento dati
     if (name.value == "" || distance.value == "" || fasciaEta.value == "") {
         alert('Attenzione: devi selezionare tutti i parametri.')
     } else {
 
+        // far apparire nome passeggero
+        nomePasseggero.innerHTML = name.value;
+
+        // far apparire tipo offerta
+        if (fasciaEta.value = 'minorenne') {
+            offerType.innerHTML = 'Biglietto ridotto';
+        } else if (fasciaEta.value = 'anziano') {
+            offerType.innerHTML = 'Biglietto ridotto';
+        } else {
+            offerType.innerHTML = 'Biglietto standard';
+        }
+
+        // far apparire numero carrozza
+
+        // far apparire codice cp
+
+        // far apparire costo biglietto
+        
     }
 })
 
